fc80ae4cf497e569eee2994c9bebdf041e8f0893
Andrey <mands315@gmail.com>
2023-04-12 14:47:37 +0300

    Add outlets and actions

---
+++ Destini/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Andrey on 12.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    //MARK: - LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Actions
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+
+
+}
+
